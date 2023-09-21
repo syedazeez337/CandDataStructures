ab8a39dfd2d7be335746f4b4ec510bc9f0a9aaa4
syedazeez337 <67051473+syedazeez337@users.noreply.github.com>
2023-09-21 13:17:03 +0530

    Update hello.c
    first "Hello, world" programme in C

--- Chapter_01/hello.c
+++ Chapter_01/hello.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(void)
+{
+    //using `puts` when printing strings
+    // can also use `printf("%s", "string");
+    puts("Hello, world!!!");
+    printf("%s", "Welcome to C\n");
+}
+
+/*
+use : gcc -Wall -Wextra -std=c2x <file>.c -o <file>
+*/
