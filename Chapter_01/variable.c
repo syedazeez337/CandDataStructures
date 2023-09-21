63f9ed18fd7e1cf88018c00682adb6e2f5dc7da8
syedazeez337 <67051473+syedazeez337@users.noreply.github.com>
2023-09-21 13:23:52 +0530

    Update variable.c

--- Chapter_01/variable.c
+++ Chapter_01/variable.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void)
+{
+    //variables in c
+    int i = 6;
+    int j = 8;
+    int k = i + j;
+    
+    printf("Sum of %d and %d is %d\n", i, j, k);
+}
+
+/*
+use : gcc -Wall -Wextra -std=c2x <file>.c -o <file>
+*/
